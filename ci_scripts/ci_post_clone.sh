2c9235fcf4f68bf6482fd6e2905c049ad5a53c56
Adam Young <me@adam-young.co.uk>
2025-12-08 15:05:01 +0000

    Update ci_post_clone.sh

--- ci_scripts/ci_post_clone.sh
+++ ci_scripts/ci_post_clone.sh
@@ -8,6 +8,6 @@ defaults write com.apple.dt.Xcode IDESkipMacroFingerprintValidation -bool YES
 # SwiftLint on Analyze action
 if [ $CI_XCODEBUILD_ACTION = 'analyze' ];
 then
-    cd $CI_WORKSPACE
+    cd $CI_PRIMARY_REPOSITORY_PATH
     swift format lint -r -p .
 fi
